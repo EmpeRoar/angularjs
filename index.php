0589b0293c994628ec4d8bf34be908534a2288dc
juliusbacosa <juliusbacosa@gmail.com>
2015-11-03 18:18:54 +0800

    angular js
    angular js

---
+++ index.php
@@ -0,0 +1,13 @@
+<html ng-app>
+	<head>
+		<title>Take My Up! AngularJs</title>
+		<meta name="description" content="AngularJs consuming asp.net webapi"/>
+		<script src="/Scripts/angular.min.js"></script>
+
+	</head>
+	<body>
+			<div>
+					Hello Angular
+			</div>	
+	</body>
+</html>
