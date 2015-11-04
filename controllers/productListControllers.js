6235f09fac71e022e8eb74b38d4371eda425b699
juliusbacosa <juliusbacosa@gmail.com>
2015-11-04 18:04:57 +0800

    add to cart
    add to cart

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -1,7 +1,7 @@
 angular.module("sportsStore")
 	   .constant("productListActiveClass", "btn-primary")
 	   .constant("productListPageCount", 3)
-	   .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
+	   .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
 	
 	var selectedCategory = null;
 	
@@ -27,5 +27,9 @@ angular.module("sportsStore")
 
 	$scope.getPageClass = function (page) {
 		return $scope.selectedPage == page ? productListActiveClass : "";
+	}
+
+	$scope.addProductToCart = function (product) {
+		cart.addProduct(product.id, product.name, product.price);
 	}
 });
