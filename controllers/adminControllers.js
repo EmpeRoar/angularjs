d810e00919a52c598d08b46caddaaf96dd496df0
juliusbacosa <juliusbacosa@gmail.com>
2015-11-04 20:23:44 +0800

    admin page
    admin page

---
+++ controllers/adminControllers.js
@@ -0,0 +1,34 @@
+/*
+angular.module("sportsStoreAdmin")
+.constant("authUrl", "http://localhost:50220/users/login")
+.controller("authCtrl", function($scope, $http, $location, authUrl) {
+	$scope.authenticate = function (user, pass) {
+		$http.post(authUrl, {
+			username: user,
+			password: pass
+		}, {
+			withCredentials: true
+		}).success(function (data) {			
+			$location.path("/main");
+		}).error(function (error) {
+			$scope.authenticationError = error;
+		});
+	}
+});
+*/
+
+angular.module("sportsStoreAdmin")
+	.constant("authUrl", "http://localhost:50220/users/login")
+	.controller("authCtrl", function($scope, $http, $location, authUrl) {
+		$location.path("/main");	
+	})
+	.controller("mainCtrl", function($scope) {
+		$scope.screens = ["Products", "Orders"];
+		$scope.current = $scope.screens[0];
+		$scope.setScreen = function (index) {
+		$scope.current = $scope.screens[index];
+	};
+	$scope.getScreen = function () {
+		return $scope.current == "Products" ? "/views/adminProducts.html" : "/views/adminOrders.html";
+	};
+});
--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,4 +1,7 @@
-angular.module("sportsStore").constant("dataUrl", "http://localhost:50220/home/Products").controller("sportsStoreCtrl", function ($scope,$http,dataUrl) {
+angular.module("sportsStore")
+	.constant("dataUrl", "http://localhost:50220/home/Products")
+	.constant("orderUrl", "http://localhost:50220/home/PutForm")
+	.controller("sportsStoreCtrl", function ($scope,$http,dataUrl,orderUrl, cart) {
 
 	
 	$scope.data = {
@@ -15,6 +18,23 @@ angular.module("sportsStore").constant("dataUrl", "http://localhost:50220/home/P
 	});
 
 	
+	$scope.sendOrder = function (shippingDetails) {
+
+		console.log(shippingDetails);
+
+		var order = angular.copy(shippingDetails);
+		order.products = cart.getProducts();
+		$http.post(orderUrl, order).success(function (data) {
+			$scope.data.orderId = data.id;
+			cart.getProducts().length = 0;
+		}).error(function (error) {
+			$scope.data.orderError = error;
+		}).finally(function () {
+			$location.path("/complete");
+		});
+	}
+
+
 });
 
 
